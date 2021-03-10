a81ddff74b32b2d4587cc381b8d61019595ad5bf
parsa_samandi <parsasamandizadeh@gmail.com>
2021-03-10 16:39:21 +0330

    Improvement

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -68,4 +68,4 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
             ]);
         });
     }
-}
+} 
--- database/migrations/2021_03_10_125006_create_cat_table.php
+++ database/migrations/2021_03_10_125006_create_cat_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubCatTable extends Migration {
+class CreateCatTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -13,11 +13,10 @@ class CreateSubCatTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('sub_cat', function(Blueprint $table)
+		Schema::create('cat', function(Blueprint $table)
 		{
 			$table->integer('id', true);
-			$table->string('name', 55);
-			$table->integer('c_id')->nullable()->default(0)->index('c_id');
+			$table->string('name');
 			$table->integer('status')->default(0);
 		});
 	}
@@ -30,7 +29,7 @@ class CreateSubCatTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('sub_cat');
+		Schema::drop('cat');
 	}
 
 }
